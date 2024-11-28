6aafd701a72a9010e5d57cb89925465b31fd3505
maivlys <122049862+maivlys@users.noreply.github.com>
2024-11-28 15:06:20 +0100

    status style changed by adding/removing classes, not styling directly

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const addBtn = document.getElementById("add-book-btn");
 const dialog = document.getElementById("dialog-form");
 const deleteBookBtn = document.querySelectorAll(".bin-btn");
+const statusBtn = document.querySelectorAll(".status");
 
 addBtn.addEventListener("click", () => {
   dialog.showModal();
@@ -10,5 +11,23 @@ deleteBookBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     let section = btn.closest("section");
     section.remove();
+  });
+});
+
+statusBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.textContent === "Not read") {
+      btn.textContent = "Reading";
+      btn.classList.remove("not-read");
+      btn.classList.add("reading");
+    } else if (btn.textContent === "Reading") {
+      btn.textContent = "Read";
+      btn.classList.remove("reading");
+      btn.classList.add("read");
+    } else if (btn.textContent === "Read") {
+      btn.textContent = "Not read";
+      btn.classList.remove("read");
+      btn.classList.add("not-read");
+    }
   });
 });
