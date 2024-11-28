4b3f332b96fc05f7d1d26119641604d1e5f7a96f
maivlys <122049862+maivlys@users.noreply.github.com>
2024-11-28 14:27:26 +0100

    delete clicked book

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const addBtn = document.getElementById("add-book-btn");
 const dialog = document.getElementById("dialog-form");
-const deleteBook = document.querySelector(".bin-btn");
+const deleteBookBtn = document.querySelectorAll(".bin-btn");
 
 addBtn.addEventListener("click", () => {
   dialog.showModal();
+});
+
+deleteBookBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let section = btn.closest("section");
+    section.remove();
+  });
 });
